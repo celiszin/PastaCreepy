24d83625a6bd25831a9953e844d79eabc8c78388
celiszin <teste153ovo@gmail.com>
2025-11-06 08:25:58 -0300

    att

---
+++ js/historias.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const idDaLenda = urlParams.get('id'); // Pega o valor do parâmetro 'id'
+
+    const container = document.querySelector('.historia-container');
+    
+    if (idDaLenda) {        
+        const todasLendas = document.querySelectorAll('.lenda-completa');
+        todasLendas.forEach(lenda => {
+            lenda.style.display = 'none';
+        });
+        const lendaParaMostrar = document.getElementById(idDaLenda);
+        if (lendaParaMostrar) {
+            lendaParaMostrar.style.display = 'block'; // Ou 'flex', dependendo do CSS
+            lendaParaMostrar.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            console.error(`Lenda com ID "${idDaLenda}" não encontrada.`);
+            todasLendas.forEach(lenda => {
+                lenda.style.display = 'block';
+            });
+        }
+        
+    } else {
+
+        console.log('Nenhum ID de lenda especificado. Exibindo todas as histórias.');
+    }
+});
